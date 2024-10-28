1ca917aad1bed70e76e31f06138bd1239ba1694a
Avashneupane9857 <juisch20220011252@gmail.com>
2024-10-29 02:31:43 +0530

    :lipstick:

--- frontend/src/routes/Routing.jsx
+++ frontend/src/routes/Routing.jsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import PropertyDetailsPage from "../pages/PropertyDetailsPage";
+import LoginPage from "../pages/LoginPage";
+import SignupPage from "../pages/SignupPage";
 
 function Routing() {
   return (
     <div className="font-serif">
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        {/* <Route path="/sigup" element />
-        <Route path="/login" element /> */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/property/details" element={<PropertyDetailsPage />} />
       </Routes>
     </div>
