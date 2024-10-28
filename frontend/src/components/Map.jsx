0ea97460ef98c6d0742f30f5e893c39ce5d95fbb
Avashneupane9857 <juisch20220011252@gmail.com>
2024-10-28 07:48:49 +0530

    Maps section

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,7 +1,23 @@
+import GoogleMapReact from "google-map-react";
 function Map() {
+  const defaultProps = {
+    center: {
+      lat: 28.3949,
+      lng: 84.124,
+    },
+    zoom: 11,
+  };
+
   return (
     <div className="relative top-56">
       <h1 className="text-2xl">Where you will be</h1>
+      <div style={{ height: "60vh", width: "100%" }} className="relative top-6">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "" }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+        ></GoogleMapReact>
+      </div>
     </div>
   );
 }
