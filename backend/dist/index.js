2d875445393ecb1fba9674af49b20da081c5a67d
Avashneupane9857 <juisch20220011252@gmail.com>
2024-12-30 00:59:34 +0530

    added cors

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -33,7 +33,7 @@ const dotenv = __importStar(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 dotenv.config();
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://your-frontend-domain.com"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://your-frontend-domain.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import * as dotenv from 'dotenv';
 import cors from "cors";
 dotenv.config();
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://your-frontend-domain.com"],
+    origin: ["http://localhost:5173","http://localhost:5174", "https://your-frontend-domain.com"],
     methods: ["GET", "POST", "PUT", "DELETE"], 
     allowedHeaders: ["Content-Type", "Authorization"], 
     credentials: true, 
